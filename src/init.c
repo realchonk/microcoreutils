0390a8ea20442c3d0467124eacf168eac771b838
Benni3D <benni@stuerz.xyz>
2021-04-08 04:07:11 +0200

    Added sync, updated init

--- src/init.c
+++ src/init.c
@@ -1,11 +1,17 @@
+#include <sys/reboot.h>
 #include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void) {
+   if (getpid() != 1) {
+      fputs("init: already running\n", stderr);
+      return 1;
+   }
    // Create /proc, /sys, /dev, /dev/pts
    mkdir("/proc", 755);
    mkdir("/sys", 755);
@@ -15,6 +21,7 @@ int main(void) {
    // Mount /proc, /sys, /dev/pts
    if (mount(NULL, "/proc", "proc", 0, NULL) < 0) perror("Failed to mount /proc");
    if (mount(NULL, "/sys", "sysfs", 0, NULL) < 0) perror("Failed to mount /sys");
+   mount(NULL, "/dev", "devtmpfs", 0, NULL); // May already be mounted by kernel
    if (mount(NULL, "/dev/pts", "devpts", 0, NULL) < 0) perror("Failed to mount /dev/pts");
 
    // Setup some environement variables
@@ -33,6 +40,12 @@ int main(void) {
       waitpid(pid, &wstatus, 0);
       if (!WIFEXITED(wstatus) || WEXITSTATUS(wstatus) != 0) break;
    }
+
+   // Write all changes to disk
+   sync();
+
+   // TODO: find portable solution
+   reboot(RB_POWER_OFF);
 
    return 0;
 }
